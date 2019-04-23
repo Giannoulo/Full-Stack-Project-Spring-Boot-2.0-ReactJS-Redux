35af79e9d176c7eae0d660e6a320e3892f23ffd7
George Giannoulopoulos <ggiannoulopoulos@konnektable.com>
2019-04-23 17:05:06 +0300

    First commit

---
+++ spring-boot/ppmtool/src/main/java/wot/giannoulo/ppmtool/PpmtoolApplication.java
@@ -0,0 +1,13 @@
+package wot.giannoulo.ppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmtoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmtoolApplication.class, args);
+	}
+
+}
