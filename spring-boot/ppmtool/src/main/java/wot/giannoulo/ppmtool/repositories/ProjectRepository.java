c50c5f33488f30376239a1ae1de834992cff83c6
George Giannoulopoulos <ggiannoulopoulos@konnektable.com>
2019-04-24 15:01:17 +0300

    Project Entity & Project Repository

---
+++ spring-boot/ppmtool/src/main/java/wot/giannoulo/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,17 @@
+package wot.giannoulo.ppmtool.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import wot.giannoulo.ppmtool.domain.Project;
+
+/**
+ * Created by George Giannoulopoulos
+ * on 24/4/2019.
+ */
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
+}
