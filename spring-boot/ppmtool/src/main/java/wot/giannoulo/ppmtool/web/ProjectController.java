6e65ec97e1feacdd8df055f59dda1a2df19d92eb
George Giannoulopoulos <ggiannoulopoulos@konnektable.com>
2019-04-25 13:33:04 +0300

    Project Validation @Valid

--- spring-boot/ppmtool/src/main/java/wot/giannoulo/ppmtool/web/ProjectController.java
+++ spring-boot/ppmtool/src/main/java/wot/giannoulo/ppmtool/web/ProjectController.java
@@ -3,12 +3,15 @@ package wot.giannoulo.ppmtool.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import wot.giannoulo.ppmtool.domain.Project;
 import wot.giannoulo.ppmtool.services.ProjectService;
+
+import javax.validation.Valid;
 
 /**
  * Created by George Giannoulopoulos
@@ -23,7 +26,11 @@ public class ProjectController {
     private ProjectService projectService;
 
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project){
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
+
+        if(result.hasErrors()){
+            return new ResponseEntity<String>( "Invalid Project Object", HttpStatus.BAD_REQUEST);
+        }
         projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
     }
