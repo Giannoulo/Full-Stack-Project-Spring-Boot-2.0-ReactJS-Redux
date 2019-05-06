280c54c2089d6ca46fc214300b8b9f5ad8a90807
George Giannoulopoulos <ggiannoulopoulos@konnektable.com>
2019-05-06 16:51:45 +0300

    Added css

--- react-redux/ppmtool-react-client/src/App.js
+++ react-redux/ppmtool-react-client/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/Layout/Header";
-import "bootstrap/dist/css/bootstrap-reboot.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
