3426dd935afdb9bf0ce57e52535ea0e4d4e3e7b8
George Giannoulopoulos <ggiannoulopoulos@konnektable.com>
2019-05-06 14:44:44 +0300

    Installed Bootstrap

--- react-redux/ppmtool-react-client/src/components/Dashboard.js
+++ react-redux/ppmtool-react-client/src/components/Dashboard.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
 import ProjectItem from "./Project/ProjectItem";
-import { directive } from "@babel/types";
 
 class Dashboard extends Component {
   render() {
     return (
       <div>
-        <h1>Welcome to the Dashboard</h1>
+        <h1 className="alert alert-warning">Welcome to the Dashboard</h1>
         <ProjectItem />
         <ProjectItem />
         <ProjectItem />
