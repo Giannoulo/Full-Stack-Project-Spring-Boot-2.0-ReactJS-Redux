1bd756110358bb570e9df71b89b24d960a1f51bc
George Giannoulopoulos <ggiannoulopoulos@konnektable.com>
2019-05-06 14:36:40 +0300

    Added header

--- react-redux/ppmtool-react-client/src/components/Dashboard.js
+++ react-redux/ppmtool-react-client/src/components/Dashboard.js
@@ -1,8 +1,17 @@
 import React, { Component } from "react";
+import ProjectItem from "./Project/ProjectItem";
+import { directive } from "@babel/types";
 
 class Dashboard extends Component {
   render() {
-    return <h1>Welcome to the Dashboard</h1>;
+    return (
+      <div>
+        <h1>Welcome to the Dashboard</h1>
+        <ProjectItem />
+        <ProjectItem />
+        <ProjectItem />
+      </div>
+    );
   }
 }
 
---
+++ react-redux/ppmtool-react-client/src/components/Project/ProjectItem.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class ProjectItem extends Component {
+  render() {
+    return <h1>Project item</h1>;
+  }
+}
+
+export default ProjectItem;
