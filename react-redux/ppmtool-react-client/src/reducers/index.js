da4a52677d33104ba2d4805baf43fd25dabed470
George Giannoulopoulos <ggiannoulopoulos@konnektable.com>
2019-05-16 16:48:39 +0300

    Added projects at the state of dashboard

--- react-redux/ppmtool-react-client/src/reducers/index.js
+++ react-redux/ppmtool-react-client/src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 import errorReducer from "./errorReducer";
+import projectReducer from "./projectReducer";
 // combinereducers is used when multiple reducers are present
 
 export default combineReducers({
-  errors: errorReducer
+  errors: errorReducer,
+  project: projectReducer
 });
