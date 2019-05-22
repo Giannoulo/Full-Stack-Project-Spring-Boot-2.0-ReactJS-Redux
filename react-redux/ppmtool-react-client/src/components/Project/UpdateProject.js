cc503b6b1c75e081f8c47c65fdc92ec224182559
George Giannoulopoulos <ggiannoulopoulos@konnektable.com>
2019-05-22 12:31:56 +0300

    Added state to update project

--- react-redux/ppmtool-react-client/src/components/Project/UpdateProject.js
+++ react-redux/ppmtool-react-client/src/components/Project/UpdateProject.js
@@ -1,6 +1,14 @@
 import React, { Component } from "react";
+import { getProject } from "../../actions/projectActions";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import classnames from "classnames";
 
 class UpdateProject extends Component {
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.getProject(id, this.props.history);
+  }
   render() {
     return (
       <div className="project">
@@ -61,4 +69,15 @@ class UpdateProject extends Component {
   }
 }
 
-export default UpdateProject;
+UpdateProject.propTypes = {
+  getProject: PropTypes.func.isRequired,
+  project: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  project: state.project.project
+});
+export default connect(
+  mapStateToProps,
+  { getProject }
+)(UpdateProject);
